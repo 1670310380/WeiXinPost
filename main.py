38347824e8479c31d05ccedd01d09f0c1796aa35
1670310380 <119231259+1670310380@users.noreply.github.com>
2024-05-06 17:38:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def get_access_token():
     app_id = config.app_id
     # appSecret
     app_secret = config.app_secret
-    post_url = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={}&secret={}"
-                .format(app_id, app_secret))
+    post_url = (https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxc40bb446bf8ea91a&secret=f60b0f850865291d003dff8b24267257)
     print(get(post_url).json())
     access_token = get(post_url).json()['access_token']
     # print(access_token)
